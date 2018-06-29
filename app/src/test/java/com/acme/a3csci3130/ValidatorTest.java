ac91236bc966b00e1c9646e3b48c5a85c0f69b5b
Alexlinsy <alexlinsy@live.com>
2018-06-29 01:25:27 -0300

    Assignment 3 code with successful Junit Test and UI Test

--- app/src/test/java/com/acme/a3csci3130/ValidatorTest.java
+++ app/src/test/java/com/acme/a3csci3130/ValidatorTest.java
@@ -9,25 +9,25 @@ public class ValidatorTest {
     Validator validator = new Validator();
     @Test
     public void nameValidate() {
-        String name = "a";
+        String name = "Ian";
         assertEquals(true, validator.nameValidate(name));
     }
 
     @Test
     public void businessNumberValidate() {
-        String number = "1234";
+        String number = "902789123";
         assertEquals(true, validator.businessNumberValidate(number));
     }
 
     @Test
     public void addressValidate() {
-        String address = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        String address = "666 Sunset Blvd";
         assertEquals(true, validator.addressValidate(address));
     }
 
     @Test
     public void emailValidate() {
-        String email = "testmail.com";
+        String email = "test@gmail.com";
         assertEquals(true, validator.emailValidate(email));
     }
 }
